dce0176c96d093bc3eb59edc853c5e93bb3363b7
George <71434803+george-swift@users.noreply.github.com>
2021-03-19 18:15:08 +0100

    Add tests for controllers and fixtures for integration test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,14 +1,9 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get posts_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get posts_create_url
-    assert_response :success
+    assert_redirected_to member_session_path
   end
 
   test "should get index" do
