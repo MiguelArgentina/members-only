d2a4b60de68b7891712a4d34a24760ec77bd9acc
Miguel Tucu Gomez <miguelgomez66@gmail.com>
2021-03-19 01:55:58 -0300

    Add Create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,23 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
