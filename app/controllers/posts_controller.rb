615f605133401836312a410f6ed9906df0c7c80d
George <71434803+george-swift@users.noreply.github.com>
2021-03-19 23:38:17 +0100

    Refactor route and action in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_member!, only: %i[new create]
 
   def new
-    @post = current_member.posts.build
+    @post = Post.new
   end
 
   def show; end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root 'posts#show'
 
   devise_for :members
-  resources :posts
+  resources :posts, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
