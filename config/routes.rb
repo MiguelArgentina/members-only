e8824a50b25d85ff58f926cfd062bb4a178c0f9e
George <71434803+george-swift@users.noreply.github.com>
2021-03-19 13:03:24 +0100

    Refactor routes and remove redundant resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   get 'posts/create'
   get 'posts/index'
 
-  root "posts#index"
+  root 'posts#show'
 
   devise_for :members
-  resources :members
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
